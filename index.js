39e9dc161fb012090ec61b6a94a74b65eb53f453
Mass1milian0 <maxymino@hotmail.it>
2021-09-26 11:05:10 +0100

    requested changes

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ function randomIntFromInterval(min, max) { // min and max included
 }
 function randomDate() {
   let today = new Date()
-  let date = new Date(randomIntFromInterval(today.getFullYear(),today.getFullYear()+20),randomIntFromInterval(0,11),randomIntFromInterval(0,20));
+  let date = new Date(randomIntFromInterval(today.getFullYear(),today.getFullYear()+20),randomIntFromInterval(today.getMonth(),11),randomIntFromInterval(today.getDay,30));
   return date;
 }
 
 let p = document.createElement("p")
 let randDate = randomDate()
+let today = date()
 p.innerText = randDate.getFullYear() + "/" + (randDate.getMonth() +1 ) + "/" + (randDate.getDay() + 1)
 document.querySelector("#appendafter").appendChild(p)
