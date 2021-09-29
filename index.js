c980642e29483982e887af7ea05bd3ddd626265f
Mass1milian0 <maxymino@hotmail.it>
2021-09-29 14:05:14 +0100

    fixed some things from the pull request merge

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ function randomIntFromInterval(min, max) { // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 function ranDate() {
-  let today = new Date()
-  let randYear = randomIntFromInterval(today.getFullYear(), today.getFullYear() + 1)
-  
+  const today = new Date()
+  const randYear = randomIntFromInterval(today.getFullYear(), today.getFullYear() + 1)
+  let month = 0
   if (randYear > today.getFullYear()) {
-    var month = 0
+    month = 0
   } else {
-    var month = today.getMonth() + 1
+    month = today.getMonth() + 1
   }
   return new Date(randYear, Math.min(randomIntFromInterval(month, 11), 11), randomIntFromInterval(0,29), 0, 0, 0, 0);
 }
